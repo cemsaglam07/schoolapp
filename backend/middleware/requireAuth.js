791b78ed24541fbc900346c1b99c8359a7a622fb
Cem Sağlam <cemsaglam07@gmail.com>
2024-07-31 13:27:18 +0300

    Auth

--- backend/middleware/requireAuth.js
+++ backend/middleware/requireAuth.js
@@ -16,3 +16,19 @@ const requireAuth = async (req, res, next) => {
 }
 
 module.exports = requireAuth;
+/*
+require('dotenv').config();
+const jwt = require("jsonwebtoken");
+
+module.exports = async (req, res, next) => {
+  try {
+    const token = await req.headers.authorization.split(" ")[1];
+    const decodedToken = await jwt.verify(token, process.env.TOKEN);
+    const user = await decodedToken;
+    req.user = user;
+    next();
+  } catch (error) {
+    res.status(401).json({error: new Error("Invalid request!")});
+  }
+};
+*/
